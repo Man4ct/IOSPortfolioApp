f44c22ef260ccf9894c672430874cec01f7d397f
fahmi.fahreza <fahmiiireza@gmail.com>
2023-04-26 23:58:19 +0800

    initial commit

---
+++ PortfolioFahmi/PortfolioFahmi/Code/Controllers/PortfolioFahmiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioFahmiApp.swift
+//  PortfolioFahmi
+//
+//  Created by Fahmi Fahreza on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioFahmiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PortfolioFahmi/PortfolioFahmi/Extensions/ColorExtensions.swift
@@ -0,0 +1,8 @@
+//
+//  ColorExtensions.swift
+//  PortfolioFahmi
+//
+//  Created by Fahmi Fahreza on 26/04/23.
+//
+
+import Foundation
