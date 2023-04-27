9c5673479eb216fde8342c1ea2bfa4e0d70c8f71
fahmi.fahreza <fahmiiireza@gmail.com>
2023-04-27 23:30:31 +0800

    adding some image

--- PortfolioFahmi/PortfolioFahmi/Code/Views/Support Views/HeaderView.swift
+++ PortfolioFahmi/PortfolioFahmi/Code/Views/Support Views/HeaderView.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 
 struct HeaderView: View {
+    var appModel : AppModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("self")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .padding(5)
+                .background(
+                Circle()
+                    .opacity(0.7)
+                    .shadow(radius: 5)
+                )
+            Text(appModel.portfolio.name)
+                .font(Montserrat.bold.font(size: 17))
+                .padding(.top, 8)
+        }
     }
 }
 
 struct HeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        HeaderView()
+        HeaderView(appModel: AppModel())
     }
 }
